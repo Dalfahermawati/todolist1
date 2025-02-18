847a96cf0081d6702e2b6f07df449a7982bda468
Dalfahermawati <dalfahermawati770@gmail.com>
2025-02-18 13:30:53 +0700

    percobaan

---
+++ main.js
@@ -0,0 +1,75 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyAjxjGgc1_HHBUGoXM1kFq4aXiV--plwZE",
+  authDomain: "pasarcemerlang-11fa3.firebaseapp.com",
+  projectId: "pasarcemerlang-11fa3",
+  storageBucket: "pasarcemerlang-11fa3.appspot.com",
+  messagingSenderId: "390685080124",
+  appId: "1:390685080124:web:6a69ed5fd39c3fc21da139",
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export async function ambilDaftarData() {
+  const refDokumen = collection(db, "todolist1");
+  const kueri = query(refDokumen, orderBy("nama"));
+  const cuplikanKueri = await getDocs(kueri);
+  let hasil = [];
+  cuplikanKueri.forEach((dok) => {
+    hasil.push({
+      id: dok.id,
+      nama: dok.data().nama,
+      prioritas: dok.data().prioritas,
+      status: dok.data().status,
+      tanggal: dok.data().tanggal,
+    });
+  });
+  return hasil;
+}
+export function formatAngka(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+export async function tambahData(nama, prioritas, status, tanggal) {
+  try {
+    const dokRef = await addDoc(collection(db, 'todolist1'), {
+      nama: nama,
+      prioritas: prioritas,
+      status: status,
+      tanggal: tanggal,
+    });
+    console.log('berhasil menambah todolist1 ' + dokRef.id);
+  }
+  catch (e) {
+    console.log('gagal menambah todolist1 ' + e);
+  }
+}
+export async function hapusData(docId) {
+  await deleteDoc(doc(db, "todolist1", docId));
+}
+
+export async function ubahData(docId, nama, prioritas, status, tanggal) {
+  await updateDoc(doc(db, "todolist1", docId), {
+    nama: nama,
+    prioritas: prioritas,
+    status: status,
+    tanggal: tanggal,
+  });
+}
+
+export async function ambilData(docId) {
+  const docRef = await doc(db, "todolist1", docId);
+  const docSnap = await getDoc(docRef);
+
+  return await docSnap.data();
+}
